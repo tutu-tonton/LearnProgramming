fc0877f02ae3267711a72cd07918fd6fd4b41e79
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2021-12-01 19:04:41 +0900

    59 会議室登録画面作成

--- FastAPI/MeetingRoomResevationAPI/app.py
+++ FastAPI/MeetingRoomResevationAPI/app.py
@@ -36,29 +36,30 @@ if page == 'users':
         st.json(res.json())
 
 elif page == 'rooms':
-    st.title('APIテスト画面（会議室)')
+    st.title('会議室登録画面')
 
     with st.form(key='room'):
-        room_id: int = random.randint(0, 10)
+        # room_id: int = random.randint(0, 10)
         room_name: str = st.text_input('会議室名', max_chars=12)
         capacity: int = st.number_input('定員', step=1)
         data = {
-            'room_id': room_id,
+            # 'room_id': room_id,
             'room_name': room_name,
             'capacity': capacity
         }
-        submit_button = st.form_submit_button(label='リクエスト送信')
+        submit_button = st.form_submit_button(label='会議室登録')
 
     if submit_button:
-        st.write('## 送信データ')
-        st.json(data)
-        st.write('## レスポンス結果')
+        # st.write('## 送信データ')
+        # st.json(data)
+        # st.write('## レスポンス結果')
         url = 'http://127.0.0.1:8000/rooms'
         res = requests.post(
             url,
             data=json.dumps(data)
         )
-        st.write(res.status_code)
+        if res.status_code == 200:
+            st.success('会議室登録完了')
         st.json(res.json())
 
 elif page == 'bookings':
